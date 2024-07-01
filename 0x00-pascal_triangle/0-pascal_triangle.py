4e09b4e5d7784f1a38cd2463f3d0aa5ff79017ab
DeborahRise <debie.rise.bookings@gmail.com>
2024-07-01 03:44:53 +0100

    first attempt on pascals triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    for i in range(n):
+        if i == 0:
+            triangle.append([1])
+        else:
+            row = [1]
+            for j in range(1, i):
+                row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+            row.append(1)
+            triangle.append(row)
+    
+    for row in triangle:
+        print(row)
+
+    return triangle
+
+# Example usage:
+pascal_triangle(5)
