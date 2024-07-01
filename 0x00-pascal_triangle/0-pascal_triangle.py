ac524f3b147e9b0defeecbce549fea947428aec9
DeborahRise <debie.rise.bookings@gmail.com>
2024-07-01 04:22:49 +0100

    diff app

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,26 +4,18 @@
 
 def pascal_triangle(n):
     """ pascal Triangle """
-    
+
     if n <= 0:
         return []
 
-    triangle = []
+    triangle = [[1]]
 
-    for i in range(n):
-        if i == 0:
-            triangle.append([1])
-        else:
-            row = [1]
-            for j in range(1, i):
-                row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-            row.append(1)
-            triangle.append(row)
-
-    for row in triangle:
-        print(row)
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            element = triangle[i - 1][j - 1] + triangle[i - 1][j]
+            row.append(element)
+        row.append(1)
+        triangle.append(row)
 
     return triangle
-
-
-pascal_triangle(5)
