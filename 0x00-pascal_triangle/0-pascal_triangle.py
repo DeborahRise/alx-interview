0d2f0b09745c54bcf31e438e5d6a73275ef91846
DeborahRise <debie.rise.bookings@gmail.com>
2024-07-01 03:59:48 +0100

    removed the code for printing each triangle row 4

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -15,6 +15,7 @@ def pascal_triangle(n):
                 row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
             row.append(1)
             triangle.append(row)
+            print(row)
 
     return triangle
 
