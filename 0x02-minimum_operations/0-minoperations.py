ec546e087e34caab3c3a904514a9cbe117ca5b98
DeborahRise <debie.rise.bookings@gmail.com>
2024-07-15 08:18:29 +0100

    pycodestyle

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -71,7 +71,7 @@ def minOperations(n):
     from the pattern above, we noticed that if the current length of characters
     can divide n without a reminder, then you can 'copyall and paste'
     else just paste
-    
+
     if not isinstance(n, int) or n <= 1:
         return 0
     file = 'H'
@@ -92,15 +92,14 @@ def minOperations(n):
     """
     if not isinstance(n, int) or n <= 1:
         return 0
-    
+
     operations = 0
     factor = 2
-    
+
     while n > 1:
         while n % factor == 0:
             operations += factor
             n //= factor
         factor += 1
-    
-    return operations
 
+    return operations
