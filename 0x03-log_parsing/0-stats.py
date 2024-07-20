d22d2f8c8b87f9ca494d9aa937accdaf64dda85c
DeborahRise <debie.rise.bookings@gmail.com>
2024-07-20 18:05:25 +0100

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,6 +5,17 @@ generates a given metrics"""
 import sys
 import re
 
-if len(sys.argv) > 1:
-    log_input = sys.argv[1]
-    valid_format = 
+""" Initialize important variables"""
+total_size = 0
+line_count = 0
+
+""" First step: compile the regular expression pattern into an object"""
+
+valid_input = re.compile(r'(\d+\.\d+\.\d+\.\d+\)\-\[d+.\] \\\"GET /projects/260 HTTP/1.1\\\" (\d+) (\d+)')
+
+""" Compare the input with the approved regex """
+for file_input in sys.stdin:
+    matching = valid_input.match(file_input)
+    if matching:
+        if line_count % 10 == 0:
+            
