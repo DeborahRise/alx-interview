0a909f21b8b06791c3cfb5c762bfbdcae161d43d
DeborahRise <debie.rise.bookings@gmail.com>
2024-07-29 04:51:44 +0100

    0-validate_utf8.py progress in the code

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 
 def validUTF8(data):
-    """ Returns True if data is a valid UTF-8 code"""
+    """ Returns True if data is a valid UTF-8 encode
+    else: You know whatsup
+    """
     number_bytes = 0
 
     mask_first = 1 << 7
     mask_second = 1 << 6
     for byte in data:
-        
+        mask = 1 << 7
+        if number_bytes == 0:
+            while mask & byte:
+                number_bytes += 1
+                mask = mask >> 1
+            if number_bytes == 1 or number_bytes > 4:
+                return False
+        else:
+            if not (byte & mask_first and not (byte & mask_second)):
+                pass
