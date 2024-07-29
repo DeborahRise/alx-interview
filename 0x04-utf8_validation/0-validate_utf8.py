d5c6803a20c6cb34e30b7cd9d89dc2f7e5cf67a3
DeborahRise <debie.rise.bookings@gmail.com>
2024-07-29 04:43:12 +0100

    0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 
 def validUTF8(data):
-    return True
+    """ Returns True if data is a valid UTF-8 code"""
+    number_bytes = 0
+
+    mask_first = 1 << 7
+    mask_second = 1 << 6
+    for byte in data:
+        
