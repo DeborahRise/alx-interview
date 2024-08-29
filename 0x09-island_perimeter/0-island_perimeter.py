dada9d43907deb6af402a8ba92714d7617b5728d
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-30 00:51:28 +0100

    checked if grid row decreases, then effect count

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -26,4 +26,35 @@ def island_perimeter(grid):
     for column in range(len(grid)):
         for row in range(len(grid[column])):
             if grid[column][row] == 1:
-                
+                if column - 1 < 0:
+                    count += 1
+                else:
+                    try:
+                        if grid[column - 1][row] == 0 or column - 1 < 0:
+                            count += 1
+                    except:
+                        pass                
+                if column + 1 > len(grid) - 1:
+                    count += 1
+                else:
+                    try:
+                        if grid[column + 1][row] == 0:
+                            count += 1
+                    except:
+                        pass
+                # check right
+                if row + 1 > len(grid[column]) - 1:
+                    count += 1
+                else:
+                    try:
+                        if grid[column][row + 1] == 0:
+                            count += 1
+                    except:
+                        pass
+                # check left
+                try:
+                    if grid[column][row - 1] == 0 or row - 1 < 0:
+                        count += 1
+                except:
+                    pass
+    return count
