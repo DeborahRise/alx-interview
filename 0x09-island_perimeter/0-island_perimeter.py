c316e82191364f4620f4ec51b6da547cd0e34f35
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-30 00:47:14 +0100

    0-island_perimeter.py-initialised my black money

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -13,3 +13,17 @@ There is only one island (or nothing).
 The island doesn’t have “lakes” (water inside that isn’t connected to the water surrounding the island).
 """
 
+def island_perimeter(grid):
+    """
+        that returns the perimeter of the island described in grid:
+
+        grid is a list of list of integers:
+        0 represents water
+        1 represents land
+        Each cell is square, with a side length of 1
+    """
+    count = 0
+    for column in range(len(grid)):
+        for row in range(len(grid[column])):
+            if grid[column][row] == 1:
+                
