9af0e62330176158e8aa2181486fbaa6b20e3261
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-30 01:12:38 +0100

    yeeeeeah

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Create a function def island_perimeter(grid): that returns the perimeter of the island described in grid:
+Create a function def island_perimeter(grid): 
+that returns the perimeter of the island described in grid:
 
 grid is a list of list of integers:
 0 represents water
@@ -10,7 +11,8 @@ Cells are connected horizontally/vertically (not diagonally).
 grid is rectangular, with its width and height not exceeding 100
 The grid is completely surrounded by water
 There is only one island (or nothing).
-The island doesn’t have “lakes” (water inside that isn’t connected to the water surrounding the island).
+The island doesn’t have “lakes” (water inside that isn’t 
+connected to the water surrounding the island).
 """
 
 def island_perimeter(grid):
