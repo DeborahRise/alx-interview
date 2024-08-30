6a6988b5e39656a49e3d589e0f5a99b9c791a77a
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-30 01:41:24 +0100

    0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Create a function def island_perimeter(grid): 
+Create a function def island_perimeter(grid):
 that returns the perimeter of the island described in grid:
 
 grid is a list of list of integers:
@@ -11,9 +11,10 @@ Cells are connected horizontally/vertically (not diagonally).
 grid is rectangular, with its width and height not exceeding 100
 The grid is completely surrounded by water
 There is only one island (or nothing).
-The island doesn’t have “lakes” (water inside that isn’t 
+The island doesn’t have “lakes” (water inside that isn’t
 connected to the water surrounding the island).
 """
+
 
 def island_perimeter(grid):
     """
@@ -34,29 +35,29 @@ def island_perimeter(grid):
                     try:
                         if grid[column - 1][row] == 0 or column - 1 < 0:
                             count += 1
-                    except:
-                        pass                
+                    except Exception:
+                        pass
                 if column + 1 > len(grid) - 1:
                     count += 1
                 else:
                     try:
                         if grid[column + 1][row] == 0:
                             count += 1
-                    except:
+                    except Exception:
                         pass
-                # check right
+                """heck right"""
                 if row + 1 > len(grid[column]) - 1:
                     count += 1
                 else:
                     try:
                         if grid[column][row + 1] == 0:
                             count += 1
-                    except:
+                    except Exception:
                         pass
                 # check left
                 try:
                     if grid[column][row - 1] == 0 or row - 1 < 0:
                         count += 1
-                except:
+                except Exception:
                     pass
     return count
